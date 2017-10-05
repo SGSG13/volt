06a472415e2b018bf694818a41b0147a2d73e682
sgsg13 <mesa5150@gmail.com>
2017-10-05 10:11:22 +0300

    # create

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+    render() {
+        return (
+            <div>Place your application here</div>
+        );
+    }
+}
+
+export default App;
